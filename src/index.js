c58eae412daf08504381a3c9f5572acad84e0fe6
Sezgin Sazlıoğulları <sezginsazlioglu@hotmail.com>
2024-06-26 20:25:08 +0300

    Updated.index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 async function enableMocking() {
-
-  if (process.env.NODE_ENV !== 'development') {
+  if (process.env.NODE_ENV !== "development") {
     return;
   }
 
-  const { worker } = await import('./mocks/browser');
+  const { worker } = await import("./mocks/browser");
   await worker.start({
     serviceWorker: {
-      url: '/mockServiceWorker.js',
+      url: "/ecommerce/mockServiceWorker.js",
+      options: {
+        scope: "/ecommerce",
+      },
     },
   });
 }
 
 enableMocking().then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-      <App />
-  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<App />);
 });
 
 reportWebVitals();
